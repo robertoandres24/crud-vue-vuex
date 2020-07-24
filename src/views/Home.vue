518339a4ba60caee7677370b9b3505c229bf163b
robertoandres24 <robertoandres24@gmail.com>
2020-07-23 21:02:15 -0400

    add pagination

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,21 @@
 <template>
   <b-container>
     <h1 class="my-3">CRUD VUE/VUEX</h1>
-    <b-table striped hover :items="people"></b-table>
+    <b-pagination
+      v-model="currentPage"
+      :total-rows="rows"
+      :per-page="perPage"
+      aria-controls="my-table"
+    ></b-pagination>
+    <p class="mt-3">Current Page: {{ currentPage }}</p>
+    <b-table
+      striped
+      hover
+      id="my-table"
+      :items="people"
+      :per-page="perPage"
+      :current-page="currentPage"
+    ></b-table>
   </b-container>
 </template>
 
@@ -16,13 +30,24 @@ export default {
   },
   data() {
     return {
+      perPage: 10,
+      currentPage: 1,
       people: []
     };
   },
+  computed: {
+    rows() {
+      return this.people.length;
+    }
+  },
   async created() {
-    const { data: people } = await this.$axios.get("people?_page=1&_limit=10");
-    console.log("created -> people", people);
-    this.people = people;
+    this.fetchPeople();
+  },
+  methods: {
+    async fetchPeople() {
+      const { data: people } = await this.$axios.get("people");
+      this.people = people;
+    }
   }
 };
 </script>
